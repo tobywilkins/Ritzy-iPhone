93e6fedcaa6e51a18fb3e13250bde9124c5661e8
Toby Wilkins <tobywilkinsuk@hotmail.co.uk>
2016-09-08 23:05:57 +0100

    exploring fb graph for photos

--- Ritzy/FBLoginView.swift
+++ Ritzy/FBLoginView.swift
@@ -11,29 +11,6 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 
 class FBLoginView: UIViewController, FBSDKLoginButtonDelegate {
-
-//    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-//        print("User Logged In")
-//        if ((error) != nil)
-//        {
-//            // Process error
-//        }
-//        else if result.isCancelled {
-//            // Handle cancellations
-//        }
-//        else {
-//            // If you ask for multiple permissions at once, you
-//            // should check if specific permissions missing
-//            if result.grantedPermissions.contains("email")
-//            {
-//                // Do work
-//            }
-//        }
-//    }
-//    
-//    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
-//        print("User Logged Out")
-//    }
     
     // Facebook Delegate Methods
     
@@ -52,13 +29,33 @@ class FBLoginView: UIViewController, FBSDKLoginButtonDelegate {
             // should check if specific permissions missing
             if result.grantedPermissions.contains("email")
             {
-                // Do work
+                self.returnUserData()
+
             }
         }
     }
     
     func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
         print("User Logged Out")
+    }
+    
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                print("Error: \(error)")
+            }
+            else
+            {
+                print("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+            }
+        })
     }
 
     override func viewDidLoad() {
@@ -73,7 +70,7 @@ class FBLoginView: UIViewController, FBSDKLoginButtonDelegate {
             let loginView : FBSDKLoginButton = FBSDKLoginButton()
             self.view.addSubview(loginView)
             loginView.center = self.view.center
-            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.readPermissions = ["public_profile", "email", "user_friends", "user_photos", "user_work_history"]
             loginView.delegate = self
         }
     }
